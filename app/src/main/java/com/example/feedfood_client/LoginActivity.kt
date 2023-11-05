132b7e1cf69ad17f6407345cf294154e1d432508
hafinamei <hafina.meilawati@gmail.com>
2023-11-05 12:51:35 +0700

    menambahkan id, email,password pada halaman login

--- app/src/main/java/com/example/feedfood_client/LoginActivity.kt
+++ app/src/main/java/com/example/feedfood_client/LoginActivity.kt
@@ -2,10 +2,17 @@ package com.example.feedfood_client
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
+    val Login = findViewById<Button>(R.id.bt_Login)
+    val daftar = findViewById<TextView>(R.id.tv_daftar)
+    val emailEditText = findViewById<EditText>(R.id.add_email)
+    val passwordEditText = findViewById<EditText>(R.id.add_pwd)
 }
