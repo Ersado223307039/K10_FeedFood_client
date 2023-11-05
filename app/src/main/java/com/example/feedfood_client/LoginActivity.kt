0cd3f320205b9d85f20e22dc5ac731dd0db02588
Ersado223307039 <ersadocb2505@gmail.com>
2023-11-05 18:11:17 +0700

    menambahkan kode login dan Sigh in

--- app/src/main/java/com/example/feedfood_client/LoginActivity.kt
+++ app/src/main/java/com/example/feedfood_client/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.feedfood_client
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -10,9 +11,24 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val Login = findViewById<Button>(R.id.bt_Login)
+        val daftar = findViewById<TextView>(R.id.tv_daftar)
+        val emailEditText = findViewById<EditText>(R.id.add_email)
+        val passwordEditText = findViewById<EditText>(R.id.add_pwd)
+
+        daftar.setOnClickListener {
+            val intent = Intent (this@LoginActivity, SignupActivity::class.java)
+            startActivity(intent)
+        }
+
+        Login.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+             startActivity(intent)
+
+            }
+        }
+
+
+
     }
-    val Login = findViewById<Button>(R.id.bt_Login)
-    val daftar = findViewById<TextView>(R.id.tv_daftar)
-    val emailEditText = findViewById<EditText>(R.id.add_email)
-    val passwordEditText = findViewById<EditText>(R.id.add_pwd)
-}
--- app/src/main/java/com/example/feedfood_client/SignupActivity.kt
+++ app/src/main/java/com/example/feedfood_client/SignupActivity.kt
@@ -1,11 +1,33 @@
 package com.example.feedfood_client
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class SignupActivity : AppCompatActivity() {
+    private fun isValidSIgnUp( email: String, password: String): Boolean {
+        return  email.isNotEmpty() && password.isNotEmpty()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        val emailEditText = findViewById<EditText>(R.id.up_email)
+        val passwordEditText = findViewById<EditText>(R.id.up_email)
+        val btDaftarButton = findViewById<Button>(R.id.bt_sign)
+
+        btDaftarButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (isValidSIgnUp(email, password)) {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
     }
 }
